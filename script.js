e166c9615aca0abeba4e8c77fc180515089c5762
Trullimom <112940668+Trullimom@users.noreply.github.com>
2023-03-19 21:09:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+var randomBtn = document.createElement("button");
+randomBtn.innerText = "Click for Ranodm Colors";
+body.appendChild(randomBtn);
+randomBtn.classList.add("randomBtn");
+
+function randomColorGenerator() {
+  color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background + ";";
+}
+
+randomBtn.addEventListener("click", randomColorGenerator);
